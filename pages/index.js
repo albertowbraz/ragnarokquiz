1d56ff3e37bed2302e037adfc2cf9f3dd6e6d6a3
albertowbraz <alberto.w.braz@gmail.com>
2021-01-25 15:41:27 -0300

    Codigo realizado ao final da primeira aula Alura

---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import db from '../db.json'; 
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px){
+  margin: auto;
+  padding: 15px;
+}`
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+
+          <Widget.Header>
+            <h1>Ragnarok</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Online</p>
+          </Widget.Content>
+      
+        </Widget>
+      
+        <Widget>
+         
+          <Widget.Header>
+            <h1>Quiz da galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+           <p>Online</p>
+          </Widget.Content>
+
+        </Widget>
+        <Footer />      
+     </QuizContainer>
+     <GitHubCorner projectUr="https://github.com/albertowbraz" />
+   </QuizBackground>
+  )
+
+}
+
